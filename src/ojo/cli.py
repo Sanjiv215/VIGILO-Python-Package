0bbddd389d0d98683080ea8b85031d3d12084984
Sanjiv Prasad <sanjivprasad360@gmail.com>
2026-09-05 13:07:35 +0530

    Testing git commits

--- src/ojo/cli.py
+++ src/ojo/cli.py
@@ -8,7 +8,6 @@ from collections.abc import Sequence
 from pathlib import Path
 
 from ojo import __version__
-#Testing Github
 from ojo.models import Severity
 from ojo.reporter import format_report
 from ojo.scanner import ScanConfig, Scanner
